e12eabeaf9bc829a168f160dcac343add24d0476
lp1407 <90288589+lp1407@users.noreply.github.com>
2021-09-10 20:42:06 -0700

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -12,5 +12,6 @@ def decrypt(key,ciphertext):
     for i in range(len(ciphertext)):
         wordHolder = ciphertext[i]
         plaintext += chr((ord(wordHolder) - 65) % 26 +65)
+        plaintext = plaintext [::-1]
         print(plaintext)
     return plaintext
