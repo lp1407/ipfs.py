a53a47c225dac1b1d9a347aef616944cf94aa061
lp1407 <90288589+lp1407@users.noreply.github.com>
2021-10-18 17:17:11 -0700

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -1,37 +1,15 @@
-import hashlib
-class Block:
-    def __init__(self, index, timestamp, content, previous_hash):
-      self.index = index
-      self.timestamp = timestamp
-      self.content = content
-      self.previous_hash = previous_hash
-      self.hash = self.calc_hash()
-   
-    def calc_hash(self):
-      sha = hashlib.sha256()
-      sha.update(str(self.index).encode('utf-8') + 
-                 str(self.timestamp).encode('utf-8') + 
-                 str(self.content).encode('utf-8') + 
-                 str(self.previous_hash).encode('utf-8'))
-      return sha.hexdigest()
-      
-M4BlockChain = []
+import math
 
-from datetime import datetime
-def create_genesis_block():
-    return Block(0, datetime.now(), "Genesis Block", "0")
-    
-M4BlockChain.append(create_genesis_block())
-
-
-# write a function `next_block` to generate a block
-def next_block(last_block):
-    index = last_block.index+1
-    next_block = Block(index, datetime.now(), f"this is block {str(index)}", last_block.hash)
-    return next_block
-    
-# append 5 blocks to the blockchain
-def app_five(block_list):
-    for i in range(5): 
-        block_list.append(next_block(block_list[-1]))
-    return block_list
+def num_BTC(b):
+    currentBlock = 1
+    currentReward = float(50)
+    c = float(50)
+    while currentBlock != b:
+        if currentBlock % 210000 == 0:
+            currentBlock = currentBlock + 1
+            currentReward = currentReward / 2
+            c = c + currentReward
+        else:
+            c = c + currentReward
+            currentBlock = currentBlock + 1
+    return c
