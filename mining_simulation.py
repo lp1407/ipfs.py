ed3c90333e01889953cdecacb3c541e1b6872967
lp1407 <90288589+lp1407@users.noreply.github.com>
2022-05-18 19:25:20 -0700

    Update mining_simulation.py

--- mining_simulation.py
+++ mining_simulation.py
@@ -1,80 +1,18 @@
-import random
+def encrypt(key,plaintext):
+     ciphertext=""
+    #YOUR CODE HERE
+    for i in range(len(plaintext)) :
+        char = plaintext[i]
+        ciphertext += chr((ord(char) + key - 65) % 26 + 65)
+    #print(ciphertext)
+    return ciphertext
 
-#alpha: selfish miners mining power (percentage),
-#gamma: the ratio of honest miners choose to mine on the selfish miners pool's block
-#N: number of simulations run
-def Simulate(alpha,gamma,N, seed):
-    
-    # DO NOT CHANGE. This is used to test your function despite randomness
-    random.seed(seed)
-  
-    #the same as the state of the state machine in the slides 
-    state=0
-    # the length of the blockchain
-    ChainLength=0
-    # the revenue of the selfish mining pool
-    SelfishRevenue=0
-
-    #A round begin when the state=0
-    for i in range(N):
-        r=random.random()
-        if state==0:
-            #The selfish pool has 0 hidden block.
-            if r<=alpha:
-                #The selfish pool mines a block.
-                #They don't publish it. 
-                state=1
-            else:
-                #The honest miners found a block.
-                #The round is finished : the honest miners found 1 block
-                # and the selfish miners found 0 block.
-                ChainLength+=1
-                state=0
-
-        elif state==1:
-            #The selfish pool has 1 hidden block.
-            if r<=alpha:
-                #The selfish miners found a new block.
-                #Write a piece of code to change the required variables.
-                #You might need to define new variable to keep track of the number of hidden blocks.
-                state+=1
-            else:
-                #Write a piece of code to change the required variables.
-                state=-1
-
-        elif state==-1:
-            #It's the state 0' in the slides (the paper of Eyal and Gun Sirer)
-            #There are three situations! 
-            #Write a piece of code to change the required variables in each one.
-            if r<=alpha:
-                SelfishRevenue+=2
-                ChainLength+=2
-                state=0
-            elif r<=alpha+(1-alpha)*gamma:
-                SelfishRevenue+=1
-                ChainLength+=2
-                state=0
-            else:
-                ChainLength+=2
-                state=0
-
-        elif state==2:
-            #The selfish pool has 2 hidden block.
-            if r<=alpha:
-                state+=1
-            else:
-                #The honest miners found a block.
-                SelfishRevenue+=2
-                ChainLength+=2
-                state=0
-        elif state>2:
-            if r<=alpha:
-                #The selfish miners found a new block
-                state+=1
-            else:
-                #The honest miners found a block
-                SelfishRevenue+=1
-                ChainLength+=1
-                state-=1
-
-    return float(SelfishRevenue)/ChainLength
+def decrypt(key,ciphertext):
+    #print(ciphertext + ":")
+    plaintext=""
+    #YOUR CODE HERE
+    for i in range(len(ciphertext)) :
+        char = ciphertext[i]
+        plaintext += chr((ord(char) - key - 65) % 26 + 65)
+    #print(plaintext)
+    return plaintext
