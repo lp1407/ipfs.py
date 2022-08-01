628466e1d1181a9099473a4aeb26bba94e1050a5
lp1407 <90288589+lp1407@users.noreply.github.com>
2022-08-01 14:31:02 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,46 +1,186 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, Integer, String, Table, Float, DateTime, Enum
-from sqlalchemy.orm import relationship
+from flask import Flask, request, g
+from flask_restful import Resource, Api
 from sqlalchemy import create_engine
+from flask import jsonify
+import json
+import eth_account
+import algosdk
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import load_only
 from datetime import datetime
-from sqlalchemy.orm import backref
-Base = declarative_base()
+import sys
 
-PLATFORMS = ['Algorand','Ethereum']
-
-class Order(Base):
-    __tablename__ = 'orders'
-    id = Column(Integer,primary_key=True)
-    receiver_pk = Column( String(256) )
-    sender_pk = Column( String(256) )
-    tx_id = Column(String(256))
-    buy_currency = Column(Enum(*PLATFORMS))
-    sell_currency = Column(Enum(*PLATFORMS))
-    buy_amount = Column(Integer,default=0)
-    sell_amount = Column(Integer,default=0)
-    signature = Column(String(256))
-    timestamp = Column(DateTime,default=datetime.now()) #When was the order inserted into the database
-    counterparty_id = Column(Integer,ForeignKey('orders.id')) #If the order was filled (or partially filled) the order_id of the order that acted as the counterparty
-    counterparty = relationship("Order",foreign_keys='Order.counterparty_id') #The Order object referenced by counterparty_id
-    filled = Column(DateTime) #When was the order filled
-    creator_id = Column(Integer,ForeignKey('orders.id')) #For derived orders: the order ID of the order that this order was derived from
-    child = relationship("Order", foreign_keys='Order.creator_id', backref=backref('creator', remote_side=[id])) #set "child" and "creator" to be the derived Order and creating Order objects associated. 
-
-class TX(Base):
-    __tablename__ = 'txes'
-    id = Column(Integer,primary_key=True)
-    platform = Column(Enum(*PLATFORMS))
-    receiver_pk = Column(String(256))
-    order_id = Column(Integer,ForeignKey('orders.id')) #The id of the order that 
-    order = relationship("Order", foreign_keys='TX.order_id' )
-    tx_id = Column(String(256))
-
-class Log(Base):
-    __tablename__ = 'log'
-    id = Column(Integer,primary_key=True)
-    logtime = Column(DateTime,default=datetime.now())
-    message = Column(String(1000))
-
+from models import Base, Order, Log
 engine = create_engine('sqlite:///orders.db')
-Base.metadata.create_all(engine)
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
 
+app = Flask(__name__)
+
+@app.before_request
+def create_session():
+    g.session = scoped_session(DBSession)
+
+@app.teardown_appcontext
+def shutdown_session(response_or_exc):
+    sys.stdout.flush()
+    g.session.commit()
+    g.session.remove()
+
+
+""" Suggested helper methods """
+def attachList(order, data):
+    data.append({
+        'sender_pk': order.sender_pk,
+        'receiver_pk': order.receiver_pk,
+        'buy_currency': order.buy_currency,
+        'sell_currency': order.sell_currency,
+        'buy_amount': order.buy_amount,
+        'sell_amount': order.sell_amount,
+        'signature': order.signature
+    })
+
+def startBuys(order):
+
+    g.session.add(order)
+    g.session.commit()
+    est_o = Fillbuys(order)
+
+    if (est_o is not None):
+        left_account = setBuys(order, est_o)
+        if (left_account is not None):
+            startBuys(left_account)
+
+    else:
+        return
+
+def Fillbuys(new_order):
+    est_o = g.session.query(Order).filter(Order.filled == None, Order.sell_currency == new_order.buy_currency, Order.buy_currency == new_order.sell_currency,((Order.sell_amount / Order.buy_amount) >= (new_order.buy_amount / new_order.sell_amount)), Order.sell_amount != Order.buy_amount, new_order.buy_amount != new_order.sell_amount)
+
+    return est_o.first()
+
+
+def setBuys(com_o, est_o):
+    com_o.filled = datetime.now()
+    est_o.filled = datetime.now()
+
+    com_o.counterparty_id = est_o.id
+    est_o.counterparty_id = com_o.id
+
+    if com_o.buy_amount > est_o.sell_amount:
+
+        account_left = com_o.buy_amount - est_o.sell_amount
+        exchange = com_o.buy_amount / com_o.sell_amount
+
+        dev_o = Order(creator_id=com_o.id, sender_pk=com_o.sender_pk,
+                                  receiver_pk=com_o.receiver_pk,
+                                  buy_currency=com_o.buy_currency,
+                                  sell_currency=com_o.sell_currency, buy_amount=account_left,
+                                  sell_amount=account_left / exchange)
+        g.session.add(dev_o)
+        g.session.commit()
+
+    elif com_o.buy_amount < est_o.sell_amount:
+
+        # Create a new order for remaining balance
+        account_left = est_o.sell_amount - com_o.buy_amount
+        exchange = est_o.sell_amount / est_o.buy_amount
+
+        dev_o = Order(creator_id=est_o.id, sender_pk=est_o.sender_pk,
+                                  receiver_pk=est_o.receiver_pk,
+                                  buy_currency=est_o.buy_currency,
+                                  sell_currency=est_o.sell_currency,
+                                  buy_amount= account_left / exchange, sell_amount=account_left)
+        g.session.add(dev_o)
+        g.session.commit()
+
+    else:
+        g.session.commit()
+
+
+def log_message(d):
+    g.session.add(Log(logtime=datetime.now(), message=json.dumps(d)))
+    g.session.commit()
+
+""" End of helper methods """
+
+
+@app.route('/trade', methods=['POST'])
+def trade():
+    print("In trade endpoint")
+    if request.method == "POST":
+        content = request.get_json(silent=True)
+        print(f"content = {json.dumps(content)}")
+        columns = ["sender_pk", "receiver_pk", "buy_currency",
+                   "sell_currency", "buy_amount", "sell_amount", "platform"]
+        fields = ["sig", "payload"]
+
+        for field in fields:
+            if not field in content.keys():
+                print(f"{field} not received by Trade")
+                print(json.dumps(content))
+                log_message(content)
+                return jsonify(False)
+
+        error = False
+        for column in columns:
+            if not column in content['payload'].keys():
+                print(f"{column} not received by Trade")
+                print(json.dumps(content))
+                log_message(content)
+                return jsonify(False)
+
+        if error:
+            print(json.dumps(content))
+            log_message(content)
+            return jsonify(False)
+
+        # Your code here
+        # Note that you can access the database session using g.session
+        signature = content['sig']
+        payload = json.dumps(content['payload'])
+        sender_public_key = content['payload']['sender_pk']
+        receiver_public_key = content['payload']['receiver_pk']
+        buy_currency = content['payload']['buy_currency']
+        sell_currency = content['payload']['sell_currency']
+        buy_amount = content['payload']['buy_amount']
+        sell_amount = content['payload']['sell_amount']
+        platform = content['payload']['platform']
+
+        # TODO: Check the signature
+        if platform == 'Algorand':
+            if algosdk.util.verify_bytes(payload.encode('utf-8'), signature, sender_public_key):
+                startBuys(Order(sender_pk=sender_public_key, receiver_pk=receiver_public_key,
+                                    buy_currency=buy_currency, sell_currency=sell_currency, buy_amount=buy_amount,
+                                    sell_amount=sell_amount, signature=signature))
+                return jsonify(True)
+            else:
+                log_message(content)
+                return jsonify(False)
+
+        # TODO: Check the signature
+        elif platform == 'Ethereum':
+            e_msg = eth_account.messages.encode_defunct(text=payload)
+            if eth_account.Account.recover_message(e_msg, signature=signature) == sender_public_key:
+                startBuys(Order(sender_pk=sender_public_key, receiver_pk=receiver_public_key,
+                                    buy_currency=buy_currency, sell_currency=sell_currency, buy_amount=buy_amount,
+                                    sell_amount=sell_amount, signature=signature))
+                return jsonify(True)
+            else:
+                log_message(content)
+                return jsonify(False)
+
+
+@app.route('/order_book')
+def order_book():
+    # Your code here
+    # Note that you can access the database session using g.session
+    data = []
+    for order in g.session.query(Order).all():
+        attachList(order, data)
+    return jsonify(data=data)
+
+
+if __name__ == '__main__':
+    app.run(port='5002')
